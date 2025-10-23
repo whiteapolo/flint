3ee49a3b06c7d9077aa7f35ce6aa5ac1dfb404f3
whiteapolo <darkpendraw@proton.me>
2025-10-23 21:28:28 +0300

    cleanup

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -2,33 +2,38 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <string.h>
 
 static bool is_number(const char *s)
 {
-    const char *curr = s;
+  if (strlen(s) == 0) {
+    return false;
+  }
 
-    while (*curr) {
-        if (!isdigit(*(curr++))) {
-            return false;
-        }
+  for (int i = 0; s[i]; i++) {
+    if (!isdigit(s[i])) {
+      return false;
     }
+  }
 
-    return true;
+  return true;
 }
 
 int builtin_exit(int argc, char **argv)
 {
-    if (argc == 1) {
-        exit(0);
-    } else if (argc == 2) {
-        if (!is_number(argv[1])) {
-            fprintf(stderr, "Argument must be a number\n");
-            exit(255);
-        }
+  if (argc == 1) {
+    exit(0);
+  }
 
-        exit(atoi(argv[1]));
-    } else {
-        fprintf(stderr, "Too many arguments\n");
-        exit(1);
+  if (argc == 2) {
+    if (!is_number(argv[1])) {
+      fprintf(stderr, "Argument must be a number\n");
+      exit(255);
     }
+
+    exit(atoi(argv[1]));
+  }
+
+  fprintf(stderr, "Too many arguments\n");
+  exit(1);
 }
